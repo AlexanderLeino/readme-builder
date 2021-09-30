1b2d792bebb76caf4ae71ba9725ecf351d9cba00
Alexander Leino <Alexander.Leino@yahoo.com>
2021-09-29 22:16:32 -0400

    another

--- 02-Homework/Develop/index.js
+++ 02-Homework/Develop/index.js
@@ -1,13 +1,95 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+const markDownGenerator = require('./utils/generateMarkdown')
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer.prompt( [
+                    {
+                        type: 'input',
+                        name: 'name',
+                        message: 'What is your first and last name'
+                        
+                    },
+                    {
+                        type: 'input',
+                        name: 'title',
+                        message: 'What is the title of your project',
 
-// TODO: Create a function to initialize app
-function init() {}
+                    },
 
-// Function call to initialize app
-init();
+                    {
+                        type: 'input',
+                        name: 'description',
+                        message: 'Write a brief description of how your project works.',
+
+                    },
+
+                    {
+                        type: 'input',
+                        name: 'installation',
+                        message: 'Please include any instructions here for being able to make your program run properly on someone elses computer',
+
+                    },
+
+                    {
+                        type: 'input',
+                        name: 'usage',
+                        message: 'What problem are you solving with the project',
+
+                    },
+
+                    {
+                        type: 'list',
+                        name: 'license',
+                        choices: ["MIT", "GPLV3", "AGPL"],
+
+                    },
+
+                    {
+                        type: 'type',
+                        name: 'contributions',
+                        message: 'Did anyone else contribute to this project? If so please indicate who.',
+                    },
+
+                    {
+                        type: 'type',
+                        name: 'tests',
+                        message: 'What is required to test your project.',
+                    },
+
+                    {
+                        type: 'type',
+                        name: 'contributions',
+                        message: 'Did anyone else contribute to this project? If so please indicate who.',
+                    },
+                    {
+                        type: 'type',
+                        name: 'questionsEmail',
+                        message: 'If anyone has any additional questions what is the best email to reach you by?.',
+                    },
+
+                    {
+                        type: 'type',
+                        name: 'questionsGithub',
+                        message: 'What is your personal github url.',
+                    },
+
+                    {
+                        type: 'type',
+                        name: 'repo',
+                        message: 'Please provide with a link to this projects repo.',
+                    },            
+                ])
+
+.then((answers) => {
+    let {year, name, title, description, installation, usage, license, contributions, questionsEmail, questionsGithub, repo} = answers
+    const markDownTemplate = markDownGenerator(answers)
+    console.log(answers)
+
+    fs.writeFile('ReadMe.Md',markDownTemplate, function(err) {
+        if(err) throw err
+        console.log('Read Me has Been Generated')
+    })
+})
+
+//Things I need help with. Getting 
--- 02-Homework/Develop/utils/generateMarkdown.js
+++ 02-Homework/Develop/utils/generateMarkdown.js
@@ -1,20 +1,113 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT'){  
+ return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+}
+  else if (license === 'GPLV3') {
+    return  `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  }
+  else if(license === 'AGPL') {
+    return `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`
+  }
+  else 
+  return ''
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseLink(license) {
+  if (license === 'MIT'){  
+    return 'www.opensource.org/licenses/MIT'
+   }
+     else if (license === 'GPLV3') {
+       return 'www.gnu.org/licenses/gpl-3.0.en.html'
+     }
+     else if(license === 'AGPL') {
+       return 'www.gnu.org/licenses/agpl-3.0.en.html'
+     }
+     else {
+       return ''
+     }
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
 
+  if (license === 'MIT'){
+    return `Copyright Enter Year Here and Enter Name Here
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
+  }
+  else if (license === 'GPLV3'){
+    
+    return `Copyright (C) Enter Year Here and Enter Name Here
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+  
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+  
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.`
+  }
+  else if(license === 'AGPL') {
+    return `Copyright (C) Enter Year Here and Enter Name Here
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.`
+  }
+}
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function generateMarkdown(answers) {
+  return`
+# ${answers.title}
+# Description
+${renderLicenseBadge(answers.license)}<br>
+${answers.description}
+# Table of Contents
+[Description](#Description)<br>
+[Installation](#Installation)<br>
+[Usage](#Usage)<br>
+[License](#License)<br>
+[Contributions](#Contributions)<br>
+[tests](#Tests)<br>
+[Questions](#Questions)<br>
+# Installation<br>
+${answers.installation} 
+# Usage
+${answers.usage} 
+# License 
+${renderLicenseSection(answers.license)}
+${renderLicenseLink(answers.license)}
+# Contributions
+${answers.contributions}
+# Tests
+${answers.tests} 
+# Questions
+${answers.questionsEmail}
+${answers.questionsGithub}
+`
 }
 
 module.exports = generateMarkdown;
